362fce161e3a09971764316e9d0f69eca623057f
arian2004 <arian2004feni@gmail.com>
2025-09-15 10:17:59 +0600

    setup: search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <>
       <section>
         <h1 className="text-center text-5xl">Hero Section</h1>
+        <SearchForm />
       </section>
     </>
   );
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchForm = () => {
+  return (
+    <div>
+      search
+    </div>
+  );
+};
+
+export default SearchForm;
