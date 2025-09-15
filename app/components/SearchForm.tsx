eac8aa310b396cdfe8fa92f9fe52ed4a1bf04e28
arian2004 <arian2004feni@gmail.com>
2025-09-15 17:37:28 +0600

    Search form modification with nextjs form

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
-const SearchForm = () => {
+import Form from 'next/form'
+ 
+export default function SearchForm() {
   return (
-    <div>
-      search
-    </div>
-  );
-};
-
-export default SearchForm;
+    <Form action="/" scroll={false} className='flex justify-center mt-5'>
+      {/* On submission, the input value will be appended to
+          the URL, e.g. /search?query=abc */}
+      <input name="query" defaultValue={""} className='bg-white text-black py-1 px-2' />
+      <button type="submit">Submit</button>
+    </Form>
+  )
+}
