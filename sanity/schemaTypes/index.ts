adbc6192cfdf4a42d0255606957f3928e28605bd
arian2004 <arian2004feni@gmail.com>
2025-09-16 15:07:47 +0600

    article, author schema added

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { author } from './author'
+import { article } from './article'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, article],
 }
