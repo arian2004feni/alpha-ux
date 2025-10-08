4c230882c71250a7d474bf7ac26e42b4d1e61131
arian2004 <arian2004feni@gmail.com>
2025-10-08 20:10:26 +0600

    author auth flow: author data saved and id modification

--- sanity/lib/query.ts
+++ sanity/lib/query.ts
@@ -1,6 +1,7 @@
 import { defineQuery } from "next-sanity";
 
-export const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+export const ARTICLES_QUERY =
+  defineQuery(`*[_type == "article" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
   _id, 
   title,
   slug,
@@ -12,9 +13,10 @@ export const ARTICLES_QUERY = defineQuery(`*[_type == "article" && defined(slug.
   description,
   category,
   image,
-}`)
+}`);
 
-export const ARTICLE_BY_ID_QUERY = defineQuery(`*[_type == "article" && _id == $id][0]{
+export const ARTICLE_BY_ID_QUERY =
+  defineQuery(`*[_type == "article" && _id == $id][0]{
   _id, 
   title,
   slug,
@@ -27,6 +29,19 @@ export const ARTICLE_BY_ID_QUERY = defineQuery(`*[_type == "article" && _id == $
   category,
   image,
   blog
-}`)
+}`);
 
-export const ARTICLE_VIEWS_QUERY = defineQuery(`*[_type == "article" && _id == $id][0]{_id, views}`)
+export const ARTICLE_VIEWS_QUERY = defineQuery(
+  `*[_type == "article" && _id == $id][0]{_id, views}`
+);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY =
+  defineQuery(`*[_type == "author" && id == $id][0]{
+    _id,
+    id,
+    name,
+    email,
+    username,
+    image,
+    bio
+  }`);
